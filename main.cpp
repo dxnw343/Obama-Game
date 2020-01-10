9805cf0a984b00759c9d2ab98afe549d4ceebdd9
TMBOZ18 <58598968+TMBOZ18@users.noreply.github.com>
2020-01-10 15:55:26 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
-#include <QGuiApplication>
+#include <QApplication>
+#include <VPApplication>
+
 #include <QQmlApplicationEngine>
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
+    VPApplication vplay;
+
+    // QQmlApplicationEngine is the preferred way to start qml projects since Qt 5.2
+    // if you have older projects using Qt App wizards from previous QtCreator versions than 3.1, please change them to QQmlApplicationEngine
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    vplay.initialize(&engine);
+
+    // use this during development
+    // for PUBLISHING, use the entry point below
+    vplay.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
+
+    // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
+    // this is the preferred deployment option for publishing games to the app stores, because then your qml files and js files are protected
+    // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
+    // also see the .pro file for more details
+    //  vplay.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
+
+    engine.load(QUrl(vplay.mainQmlFileName()));
 
     return app.exec();
 }
